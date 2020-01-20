0c4c712631f0ec291c9662adb8cec9c254377fd9
Arda Hatunoglu <ardahatunoglu@gmail.com>
2020-01-20 05:32:48 +0100

    oh yeas python babysteps
    made a python script to read csv files and give out the torque load

--- python/serialtrialwithpython.py
+++ python/serialtrialwithpython.py
@@ -1,5 +1,12 @@
-# car parameter initialization
+import scipy
 import math
+import numpy as np
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+from numpy import diff
+
+# car parameter initialization
 
 m = 1235  # kg mass of the vehicle
 g = 9.81  # m/s^2 gravitational accelaration
@@ -28,3 +35,30 @@ print(C_3)
 print(D_1)
 print(D_2)
 print(D_3)
+
+t = [] # time values
+n = [] # rpm values
+T = [] # torque values
+
+with open('wltp_whole.csv', 'r') as f: #reading the csv file
+    csvReader = csv.reader(f, delimiter=';')
+    for row in csvReader:
+        t.append(row[0])
+        n.append(row[2])
+dt=1
+n=np.asfarray(n,float) #conversion to numpy float
+dn=np.diff(n)/dt
+t[0]=str(0.0)
+t=np.asfarray(t,float)
+
+dn = np.append(dn, [0.0])
+
+T=n**2*D_1+D_2+dn*D_3
+
+
+plt.plot(t,T)
+plt.xlabel('Time(s)')
+plt.ylabel('Torque(N.m)')
+plt.show()
+
+print(T)
